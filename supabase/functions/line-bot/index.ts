ad112fa81caa0ce80007bfc2f437e9ac2cae0f2b
NPH <nph@saontour.dev>
2026-06-21 16:27:32 +0700

    fix: add Request type to serve parameter req to resolve IDE type error

--- supabase/functions/line-bot/index.ts
+++ supabase/functions/line-bot/index.ts
@@ -9,7 +9,7 @@ const corsHeaders = {
     "authorization, x-client-info, apikey, content-type, x-supabase-client-platform, x-supabase-client-platform-version, x-supabase-client-runtime, x-supabase-client-runtime-version",
 };
 
-serve(async (req) => {
+serve(async (req: Request) => {
   // Handle preflight requests
   if (req.method === "OPTIONS") {
     return new Response(null, { headers: corsHeaders });
